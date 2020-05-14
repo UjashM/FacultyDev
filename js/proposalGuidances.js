360185efaf86169969a79548539c76abafbd71d2
Vijay.Yeruva <Yeruva.Vijay@gmail.com>
2020-05-14 15:20:27 -0400

    modified proposal preparation from drop down to pill tabs

--- js/proposalGuidances.js
+++ js/proposalGuidances.js
@@ -21,24 +21,77 @@ request.onload = function(){
     'National Institute of Justice (NIJ)',
     'Substance Abuse and Mental Health Services Administration (SAMHSA)',
     'U.S. Department of Agriculture (USDA)'
-]
+    ]
+    let agencyAcronyms =    [{Name:'National Science Foundation (NSF)​', Acronym:'NSF'},
+                            {Name:'National Institutes of Health (NIH)', Acronym:'NIH'},
+                            {Name:'U.S. Department of Defense (DoD)', Acronym:'DOD'},
+                            {Name:'U.S. Department of Energy (DOE)', Acronym:'DOE'},
+                            {Name:'U.S. Department of Education (ED)', Acronym:'ED'},
+                            {Name:'National Aeronautics and Space Administration (NASA)', Acronym:'NASA'},
+                            {Name:'National Oceanic and Atmospheric Administration (NOAA)', Acronym:'NOAA'},
+                            {Name:'National Endowment for the Arts (NEA)', Acronym:'NEA'},
+                            {Name:'National Endowment for the Humanities (NEH)', Acronym:'NEH'},
+                            {Name:'National Institute of Justice (NIJ)', Acronym:'NIJ'},
+                            {Name:'Substance Abuse and Mental Health Services Administration (SAMHSA)', Acronym:'SAMHSA'},
+                            {Name:'U.S. Department of Agriculture (USDA)', Acronym:'USDA'}];
     let content = '';
     const proposalGuidances = request.response;
-    let accordionCounter = 1;
     //condition for checking if browser is Internet Explorer
     let proposalGuidance =  ((false || !!document.documentMode))? JSON.parse(proposalGuidances): proposalGuidances;
     let distinctAgencies = getDistinctAttributes(proposalGuidance, 'agency');
     distinctAgencies = customSort(agencies_sort, distinctAgencies);
-    createDropDownOptions(distinctAgencies);
-   
+
+    let navContent = createAgencyNavigation(distinctAgencies, agencyAcronyms);
+    let tabContent = buildTabContent(distinctAgencies, proposalGuidance, headers_sort);
+    appendMainContent(maincontentContainer, navContent + tabContent); 
+}
+
+let createAgencyNavigation = function(distinctAgencies, agencyAcronyms)
+{
+    let navigationContent = '<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">';
+    for(let i = 0; i< distinctAgencies.length; i++)
+    {
+        let buttonContent = '';
+        let agencyId = "agency" + i.toString();
+        let acronyms = agencyAcronyms.filter(function(agencyAcronym){
+            return agencyAcronym.Name == distinctAgencies[i].trim();
+        })
+        let acronym = acronyms[0].Acronym;
+        if(i == 0)
+        {
+            buttonContent = '<a class="nav-link active" id="pills-'+ agencyId +'-tab" data-toggle="pill" href="#pills-'+ agencyId +'" role="tab" aria-controls="pills-'+ agencyId +'" aria-selected="true">'+ acronym +'</a>';
+        }
+        else
+        {
+            buttonContent = '<a class="nav-link" id="pills-'+ agencyId +'-tab" data-toggle="pill" href="#pills-'+ agencyId +'" role="tab" aria-controls="pills-'+ agencyId +'" aria-selected="true">'+ acronym +'</a>';
+        }
+       
+        let linkElement = '<li class="nav-item">' + buttonContent + '</li>';
+        navigationContent = navigationContent + linkElement;
+    }
+    navigationContent += '</ul>';
+    return navigationContent;
+}
+
+let buildTabContent = function(distinctAgencies, proposalGuidance, headers_sort){
+    let tabContent = '<div class="tab-content" id="pills-tabContent">';
     
     for(let i = 0; i< distinctAgencies.length; i++)
     {
-        let agencyCounter = "option" + (i+1);
+        let agencyId = "agency" + i.toString();
         let agencyGuidance = proposalGuidance.filter(function(guidance){
             return guidance.agency == distinctAgencies[i];
         });
-        
+
+        if(i == 0)
+        {
+            tabContent +='<div class="tab-pane fade show active" id="pills-'+ agencyId +'" role="tabpanel" aria-labelledby="pills-'+ agencyId +'-tab">';
+        }
+        else
+        {
+            tabContent +='<div class="tab-pane fade" id="pills-'+ agencyId +'" role="tabpanel" aria-labelledby="pills-'+ agencyId +'-tab">';
+        }
+        let accordionCounter = 1;
         let distinctHeaders = getDistinctAttributes(agencyGuidance, 'mainheader');
         let accordionElemContent = '';
         distinctHeaders = customSort(headers_sort, distinctHeaders);
@@ -68,28 +121,10 @@ request.onload = function(){
            accordionElemContent+= generateAccordionElem(headerId, headingId, header, linkcontent);
            accordionCounter++;
         });
-        content = content + wrapAccordionContent(accordionElemContent, agencyCounter);
+        tabContent = tabContent + wrapAccordionContent(accordionElemContent) + '</div>';
     }
-    document.getElementById('drop-down-containers').innerHTML = content.trim();
-}
-
-let wrapAccordionContent = function(accordionElemContent, agencyCounter)
-{
-    let content = '<div class = "link-container" id = "'+ agencyCounter +'">'+
-    '<div class = "accordion" id = "accordionExample">'+ accordionElemContent + '</div></div>';
-    return content;
-}
-
-let createDropDownOptions = function(distinctAgencies)
-{
-    let dropdownOption = '';
-    for(let i = 0; i< distinctAgencies.length; i++)
-    {
-        let dataAttribute = "option"+ (i+1);
-        dropdownOption = dropdownOption + '<option data-selected = "' + dataAttribute + 
-        '">' +  distinctAgencies[i] +'</option>';
-    }
-    document.getElementById('dropdown').innerHTML = dropdownOption.trim();
+    tabContent += '</div>';
+    return tabContent;
 }
 
 let buildLinkContent = function(guidance){
@@ -101,5 +136,11 @@ let buildLinkContent = function(guidance){
         content = content + staticTextContent;
     }
     content = content + '</ul>';
+    return content;
+}
+
+let wrapAccordionContent = function(accordionElemContent)
+{
+    let content = '<div class = "accordion" id = "accordionExample">'+ accordionElemContent + '</div>';
     return content;
 }
